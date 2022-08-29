4e416fd60db4d646d0ff44ca62335692d000de59
Florian Houenou <88054514+enFloo@users.noreply.github.com>
2022-08-29 03:01:40 -0500

    success connecting mongodb database

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(proccess.env.MONGO_URI, {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false
+            useUnifiedTopology: true
+    
         })
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     }catch(err) {
