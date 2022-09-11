47542727c3fddfa0d97e23d48f4d1f5a515eb0f8
Florian Houenou <88054514+enFloo@users.noreply.github.com>
2022-09-11 01:49:13 -0500

    set up ejs, and index route

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const express = require('express');
-const dotenv = require('dotenv')
+const app = express();
+const dotenv = require('dotenv');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
 const connectDB = require('./config/db')
 
 //Load config 
 dotenv.config({ path: './config/config.env'});
 
+//setting the view engine at EJS
+app.set('view engine', 'ejs');
+
+//root views directory to public
+app.set('views', 'public');
+
+//tells express public folder is statci folder
+app.use(express.static('public'));
+
+
 connectDB()
 
-
-const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+
+//setting home route
 app.get('/', function(req, res) {
-  res.send('Hello World!!')
+  res.render('./pages/index');
 });
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`)
