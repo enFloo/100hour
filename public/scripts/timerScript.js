3c10dd3a461f890aeada930d5dc29eb3ccdffd87
Florian Houenou <88054514+enFloo@users.noreply.github.com>
2022-10-31 17:07:15 -0500

    all timer control buttons work

--- public/scripts/timerScript.js
+++ public/scripts/timerScript.js
@@ -1,161 +1,94 @@
+function format_timer(totalSeconds) {
+    // From: https://stackoverflow.com/a/25279399
+    var date = new Date(0);
+    date.setSeconds(totalSeconds);
+    var timeString = date.toISOString().substring(14, 19);
+    console.log(timeString)
+    return timeString;
+}
 
-//add zeros and colon to timer active/break time
-window.addEventListener("load", editInput(), true); function editInput(){
-    //grabbing values for timer function
-    let activeTime = document.getElementById('activeTime').textContent;
-    let breakTime = document.getElementById('breakTime').textContent;
+function update_timer() {
+    if (timeRemaining > 0) {
+        timeRemaining = timeRemaining - 1;
+    }
+    timerDisplayElm.innerText = format_timer(timeRemaining);
+    update_circle();
+}
 
-
-    if(activeTime.length == 4){
-        activeTime = activeTime.replace(/(.{2})$/,':$1')
-        document.getElementById('activeTime').innerHTML = activeTime
+function update_circle() {
+    let currentAngle = (timeRemaining / sessionActiveTime) * 360;
     
-    }else{
-        activeTime = activeTime.padStart(4, '0').replace(/(.{2})$/,':$1')
-        document.getElementById('activeTime').innerHTML = activeTime
+    // Active progress indicator
+    if (currentAngle > 180) {
+        semiCircleElements[0].style.transform = 'rotate(180deg)';
+        semiCircleElements[1].style.transform = `rotate(${currentAngle}deg)`;
+        semiCircleElements[2].style.display = 'none';
+        timerDisplayElm.style.color = '#088b8b';
+    } else {
+        semiCircleElements[0].style.transform = `rotate(${currentAngle}deg)`;
+        semiCircleElements[1].style.transform = `rotate(${currentAngle}deg)`;
+        semiCircleElements[2].style.display = 'block';
     }
 
-    if(breakTime.length == 4){
-        breakTime = breakTime.replace(/(.{2})$/,':$1');
-        document.getElementById('breakTime').innerHTML = breakTime
-    
-    }else{
-        breakTime = breakTime.padStart(4, '0').replace(/(.{2})$/,':$1')
-        document.getElementById('breakTime').innerHTML = breakTime
+    // 5sec-condition
+    if (timeRemaining <= 5) {
+        semiCircleElements[0].style.backgroundColor = 'red';
+        semiCircleElements[1].style.background = 'red';
+        timerDisplayElm.style.color = 'red';
     }
 
-};
-
-window.addEventListener('load', app(), true); function app(){
-    const semiCircles = document.querySelectorAll('.semiCircle');
-    const timer = document.querySelector('.timer')
-
-    //input
-    
-    const breakMin = 0;
-    const breakSec = 0;
-
-    //grabbing timer instructions from Timer-show page
-    activeTime = document.getElementById('activeTime').textContent
-    breakTime = document.getElementById('breakTime').textContent
-
-    
-    //turning active timer instructions into numbers and variables
-    const activeMin = Number(activeTime.slice(0,2));
-    const activeSec = Number(activeTime.slice(3,5));
-   
-
-    const activeMinutes = activeMin * 60000;
-    const activeSeconds = activeSec * 1000;
-    const breakMinutes = breakMin * 60000;
-    const breakSeconds = breakSec * 1000;
-    const activeSetTime = activeMinutes + activeSeconds;
-    const breakSetTime = breakMinutes + breakSeconds;
-    const starTime = Date.now();
-    const activeFutureTime = starTime + activeSetTime;
-    const breakFutureTime = starTime + breakSetTime;
-
-    const timerLoop = setInterval(countDownTimer);
-    countDownTimer();
-
-    function countDownTimer(){
-        const currentTime = Date.now()
-        const activeRemainingTime = activeFutureTime - currentTime;
-        const breakRemainingTime = breakFutureTime - currentTime;
-        const activeAngle = (activeRemainingTime / activeSetTime) * 360;
-        const breakAngle = (breakRemainingTime / breakSetTime) * 360;
-
-            
-        // Active progress indicator
-        if(activeAngle > 180){
-            semiCircles[2].style.display = 'none';
-            semiCircles[0].style.transform = 'rotate(180deg)';
-            semiCircles[1].style.transform = `rotate(${activeAngle}deg)`;
-            timer.style.color = '#088b8b';
-        }else{
-            semiCircles[2].style.display = 'block';
-            semiCircles[0].style.transform = `rotate(${activeAngle}deg)`;
-            semiCircles[1].style.transform = `rotate(${activeAngle}deg)`;
-        }
-
-
-        //timer
-        const activeMin = Math.floor((activeRemainingTime / (1000 * 60)) % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-        const activeSec = Math.floor((activeRemainingTime/ 1000) % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-
-    
-
-        timer.innerHTML = `
-        <div> ${activeMin}</div>
-        <div class="colon">:</div>
-        <div> ${activeSec}</div>`;
-
-        //5sec-condition
-        if(activeRemainingTime <= 6000){
-            semiCircles[0].style.backgroundColor = 'red';
-            semiCircles[1].style.background = 'red';
-            timer.style.color = 'red';
-        }
-
-        //end
-        if(activeRemainingTime < 0){
-            clearInterval(timerLoop);
-            semiCircles[0].style.display = 'none';semiCircles[1].style.display = 'none';semiCircles[2].style.display = 'none';
-
-            timer.innerHTML = `
-            <div>00</div>
-            <div class="colon">:</div>
-            <div>00</div>`;
-
-            timer.style.color = 'lightgray'
-
-        }
-
+    if (timeRemaining <= 0) {
+        clearInterval(timerInterval); // Clear the timer loop
+        semiCircleElements[0].style.display = 'none';
+        semiCircleElements[1].style.display = 'none';
+        semiCircleElements[2].style.display = 'none';
+        timerDisplayElm.style.color = 'lightgray';
     }
 }
 
+function pause_timer() {
+    if (!isPaused) {
+        clearInterval(timerInterval); // Clear the timer loop
+        isPaused = true;
+    }
+}
 
-window.addEventListener('laod', formatTimeLeft(), true); function formatTimeLeft(){
-    let activeMinutes = 0;
-    let activeSeconds = 0; 
-    let activeTotalSeconds = 0;
-    let breakMinutes = 0;
-    let breakSeconds = 0; 
-    let breakTotalSeconds = 0;
+function resume_timer() {
+    if (isPaused) {
+        update_timer();
+        timerInterval = setInterval(update_timer, 1000);
+        isPaused = false;
+    }
+}
+
+function restart_timer() {
+    pause_timer();
+    timeRemaining = sessionActiveTime;
+    resume_timer();
     
-
-    //grabbing timer instructions from Timer-show page
-    activeTime = document.getElementById('activeTime').textContent
-    breakTime = document.getElementById('breakTime').textContent
-
-    //turning active timer instructions into numbers and variables
-    activeMinutes = Number(activeTime.slice(0,2));
-    activeSeconds = Number(activeTime.slice(3,5));
-
-    //getting total active seconds
-    activeTotalSeconds = (activeMinutes * 60) + activeSeconds;
-
-
-
-    //turning break timer instructions into numbers and variables
-    breakMinutes = Number(breakTime.slice(0,2));
-    breakSeconds = Number(breakTime.slice(3,5));
-
-    //getting total break seconds
-    breakTotalSeconds = (breakMinutes * 60) + breakSeconds;
-
-
-    //adding padded 0's
-    if(activeMinutes <10){
-        activeMinutes = `0${activeMinutes}`;
-    }
-
-    if(activeSeconds <10){
-        activeSeconds = `0${activeSeconds}`;
-    }
-
-    return  `${activeMinutes}:${activeSeconds}`;
 }
 
 
 
+//
+// Global state
+//
+let isPaused = true;
+let timeRemaining = null;
+let timerInterval = null;
+const timerDisplayElm = document.getElementById('timerDisplay');
+const semiCircleElements = document.querySelectorAll('.semiCircle');
+
+window.addEventListener('load', app(), true);
+
+function app() {
+    timeRemaining = sessionActiveTime;
+
+    timerDisplayElm.innerText = format_timer(timeRemaining);
+    update_circle();
+
+    // Handle pause and resume buttons
+    document.getElementById('startButton').onclick = resume_timer;
+    document.getElementById('pauseButton').onclick = pause_timer;
+    document.getElementById('restartButton').onclick = restart_timer;
+}
