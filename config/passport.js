6e0149392801202d21351fd80526bd3f49644eaa
Florian Houenou <88054514+enFloo@users.noreply.github.com>
2022-11-05 17:15:03 -0500

    mobile css clean up

--- config/passport.js
+++ config/passport.js
@@ -7,6 +7,7 @@ module.exports = function(passport){
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: 'http://localhost:3000/auth/auth/google/callback',
+        // "https://CountDown-Interval-Timer.enfloo.repl.co/auth/auth/google/callback"
     },
     async (accessToken, refreshToken, profile, done) =>{
         const newUser = {
