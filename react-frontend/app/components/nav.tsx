853f7e5a895b13eff07b43f1de9cb7e068d62c57
Florian Houenou <floho2016@yahoo.com>
2024-11-08 11:01:04 +0000

    navbar aesthetically coming together

--- react-frontend/app/components/nav.tsx
+++ react-frontend/app/components/nav.tsx
@@ -4,14 +4,14 @@ import Link from 'next/link'
 
 export function Navbar() {
   return (
-    <nav className="grid gap-x-60 grid-cols-2 navbar border-2 border-rose-500">
+    <nav className="content-center bg-white h-14 grid gap-x-60 grid-cols-2 navbar border-2 border-rose-500">
       <div>
-        <h1>CircuitFlo</h1>
+        <h1 className="ml-5 text-xl">CircuitFlo</h1>
       </div>
-      <div id='toggleMenu'>
-        <div className="mb-1 w-9 h-1 bg-black rounded-full before:content-[‘’] before:absolute before:w-16 before:h-2 before:bg-black before:rounded-full before:-translate-y-4 after:content-[‘’] after:absolute after:w-16 after:h-2 after:bg-black after:rounded-full after:translate-y-4"></div>
-        <div className="mb-1 w-9 h-1 bg-black rounded-full before:content-[‘’] before:absolute before:w-16 before:h-2 before:bg-black before:rounded-full before:-translate-y-4 after:content-[‘’] after:absolute after:w-16 after:h-2 after:bg-black after:rounded-full after:translate-y-4"></div>
-        <div className="mb-1 w-9 h-1 bg-black rounded-full before:content-[‘’] before:absolute before:w-16 before:h-2 before:bg-black before:rounded-full before:-translate-y-4 after:content-[‘’] after:absolute after:w-16 after:h-2 after:bg-black after:rounded-full after:translate-y-4"></div>
+      <div id='toggleMenu' className="ml-3">
+        <div className="mb-1 w-9 h-1 bg-black rounded-full"></div>
+        <div className="mb-1 w-9 h-1 bg-black rounded-full"></div>
+        <div className="mb-1 w-9 h-1 bg-black rounded-full"></div>
       </div>
     </nav>
   );
