d9421567994bca89c2f2a3eea6b4e854798d002f
Florian Houenou <floho2016@yahoo.com>
2024-11-11 14:45:39 +0000

    fix main card

--- react-frontend/app/dashboard/page.tsx
+++ react-frontend/app/dashboard/page.tsx
@@ -11,8 +11,8 @@ export default function DashBoard() {
         {/* Main Content */}
         <main className="flex-1 flex mt-5 justify-center">
           {/* Main Card */}
-          <div className="w-11/12 rounded overflow-hidden shadow-lg">
-            <div className="px-6 py-4 bg-white">
+          <div className="w-11/12 ">
+            <div className="px-6 py-4 bg-white shadow-lg rounded overflow-hidden">
               <div className="text-xl mb-2">Dashboard</div>
                 <p className="text-gray-700 text-base">
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
